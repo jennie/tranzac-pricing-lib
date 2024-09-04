f7a613ce1b260b177b3d4903fbf5bc9600622551
Jennie Robinson Faber <jennie@jenniefaber.com>
2024-09-04 11:25:39 -0400

    Improvements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 export * from "./models/index.js";
-export { default as PricingRules } from "./pricingRules.js";
 export { useRoomMapping } from "./composables/useRoomMapping.js";
+
+// Export PricingRules as default
+import PricingRules from "./pricingRules.js";
+export default PricingRules;
